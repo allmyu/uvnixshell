e8c3e07357c8cead43d6a5a446973962c4be0090
allmyu <asemenh+git@gmail.com>
2025-08-05 11:55:03 -0500

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,17 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import nixpkgs { /*config = {}; overlays = [];*/ };
 in
 
 pkgs.mkShellNoCC {
+#all this dose is run uv init in the cwd then creates a virtual eniroment for python 3.12, then runs the echo
 	shellHook =
 	  ''
-	    echo "Hello shell"
-      uv init .
+    	    uv init .
 	    uv venv .venv --python 3.12
-	  '';
+	    echo "Hello shell"
+
+	'';
 	
   #packages = with pkgs; [];
 }
